2440611a74e5b847cfa1eec005062e2ae7a3a7e1
belly <67766715+belly-cyber@users.noreply.github.com>
2023-12-23 21:31:43 -0500

    Update swiss_encoder.py

--- swiss_encoder.py
+++ swiss_encoder.py
@@ -6,7 +6,6 @@ from urllib.parse import quote_plus as url_encode
 If you ever need to convert one of your oneliner payload into a 'safe' format and 
 had to do it manually I feel your pain. Thats why I have created this tool.
 '''
-
 helpmsg = '''
 swiss_encoder, the encoding swiss army knife of oneliners.
 
@@ -19,13 +18,14 @@ ACTIONS:
     base64     = base64 for linux (UTF-8)
     win64      = base64 for WINDOWS(UTF-16LE)
     url        = url safe encodeing for web interactions 
+    decimal    = gives the UTF-8 numerical value for each character
+    hex        = gives the UTF-8 HEX value for each character
 
 oneliner       = the payload that needs to be encoded. 
 '''
 
-#argv = ['winbase64', 'write-host hello world']
 arguments = argv[1:]
-option  = arguments.pop(0)
+option    = arguments.pop(0)
 
 if len(arguments) >= 1:
     payload = ' '.join(arguments)
@@ -38,12 +38,20 @@ if option == 'url':
 
 elif option == 'base64':
     bpayload   = payload.encode()
-    b64payload = base64.b64encode(bpayload)
+    b64payload = base64_encode(bpayload)
     print(b64payload.decode())
 
 elif option == 'win64':
     bpayload   = payload.encode('utf-16le')
     b64payload = base64_encode(bpayload)
     print("powershell -e", b64payload.decode())
+
+elif option == 'decimal':
+    print(','.join([str(ord(e)) for e in payload]))
+
+elif option == 'hex':
+    print(','.join([hex(ord(e)).lstrip('x0') for e in payload]))
+
 else:
     print(helpmsg)
+
